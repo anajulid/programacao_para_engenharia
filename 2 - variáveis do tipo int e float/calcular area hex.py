5c135b15e0ebaa9068d98983dc1fa4a6ed9ecd4d
anajulid <124842977+anajulid@users.noreply.github.com>
2024-03-21 22:03:43 -0300

    Update and rename area hex.py to calcular area hex.py

--- 2 - variáveis do tipo int e float/calcular area hex.py
+++ 2 - variáveis do tipo int e float/calcular area hex.py
@@ -1,7 +1,7 @@
 #raio do hex
 r = float(input("raio do hexágono: "))
 
+#calcula a área de um dos seis triângulos equiláteros que formam um hexágono
 area = (r**2 * 1.73) / 4
 
 print ("área do hexágono: ",area*6)
-          
