867a002a44bbfd1fbdd4402458551539b35f09ae
anajulid <124842977+anajulid@users.noreply.github.com>
2024-03-21 22:08:49 -0300

    Update combustível.py

--- 2 - variáveis do tipo int e float/combustível.py
+++ 2 - variáveis do tipo int e float/combustível.py
@@ -1,6 +1,7 @@
 #pergunta ao usuário quanto de dinheiro ele pretende usar com combustível
 dinheiro = float(input("quanto de dinheiro você pretende gastar com combustível?" ))
 
+#calcula quantos km o carro consegue rodar, considerando que a gasolina custe 4,95 e o carro ande 20 km/l
 km = (dinheiro / 4.95) * 20
 
 print ("a quantidade de km que seu carro consegue rodar com essa quantidade de combustível é:" ,km)
