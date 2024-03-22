70ae561abf5fae84b8d04077bc4a15074a252fe7
anajulid <124842977+anajulid@users.noreply.github.com>
2024-03-21 22:09:52 -0300

    Update desconto.py

--- 2 - variáveis do tipo int e float/desconto.py
+++ 2 - variáveis do tipo int e float/desconto.py
@@ -1,3 +1,5 @@
+#calcula o preço final de um produto considerando que ele tenha 20% de desconto
+
 preco = float(input("preço original do produto: "))
 desconto = preco * 20/100
 preco_desc = preco - (preco * 20/100)
